0649b208553d75332e3d2d56cfae50d0e234f7c1
Zeng Ruoyu <ruoyu.zeng@shopee.com>
2023-12-18 07:49:56 +0800

    Support IPs in batch

--- cli.py
+++ cli.py
@@ -13,12 +13,12 @@ logger = logging.getLogger(__name__)
 
 
 class Instance:
-    def __init__(self, ip: str, ports: List[int]) -> None:
-        self.ip = ip
+    def __init__(self, ips: List[str], ports: List[int]) -> None:
+        self.ips = ips
         self.ports = ports
 
     def __repr__(self):
-        return f'{self.ip}:{self.ports}'
+        return f'{self.ips}:{self.ports}'
 
     def __str__(self):
         return self.__repr__()
@@ -28,7 +28,12 @@ def cli(jump_host: str, config_file: str):
     instances = []
     with open(config_file) as f:
         for item in json.load(f):
-            instances.append(Instance(item['ip'], item['ports']))
+            ips = []
+            if 'ip' in item:
+                ips.append(item['ip'])
+            if 'ips' in item:
+                ips = ips + item['ips']
+            instances.append(Instance(ips, item['ports']))
     connect(jump_host, instances)
 
 
@@ -48,31 +53,34 @@ def print_instances(instances: List[Instance]):
 def add_local_interfaces(instances: List[Instance]):
     logger.info(' * adding interface, user password might be needed')
     for i in instances:
-        if sys.platform == 'darwin':
-            cmd = ['sudo', 'ifconfig', 'lo0', 'alias', i.ip]
-        else:
-            cmd = ['sudo', 'ip', 'add', 'a', 'dev', 'lo', i.ip]
-        logger.info("\tCommand %s", cmd)
-        subprocess.call(cmd)
+        for ip in i.ips:
+            if sys.platform == 'darwin':
+                cmd = ['sudo', 'ifconfig', 'lo0', 'alias', ip]
+            else:
+                cmd = ['sudo', 'ip', 'add', 'a', 'dev', 'lo', ip]
+            logger.info("\tCommand %s", cmd)
+            subprocess.call(cmd)
 
 
 def remove_local_interfaces(instances: List[Instance]):
     logger.info(' * removing interface, user/root password might be needed')
     for i in instances:
-        if sys.platform == 'darwin':
-            cmd = ['sudo', 'ifconfig', 'lo0', '-alias', i.ip]
-        else:
-            cmd = ['sudo', 'ip', 'del', 'a', 'dev', 'lo', i.ip]
-        logger.info("\tCommand %s", cmd)
-        subprocess.call(cmd)
+        for ip in i.ips:
+            if sys.platform == 'darwin':
+                cmd = ['sudo', 'ifconfig', 'lo0', '-alias', ip]
+            else:
+                cmd = ['sudo', 'ip', 'del', 'a', 'dev', 'lo', ip]
+            logger.info("\tCommand %s", cmd)
+            subprocess.call(cmd)
 
 
 def connect_ssh_tunnel(jump_host: str, instances: List[Instance]):
     logger.info(' * connecting to jump host %s', jump_host)
     opts = []
     for i in instances:
-        for p in i.ports:
-            opts += ['-L', f'{i.ip}:{p}:{i.ip}:{p}']
+        for ip in i.ips:
+            for port in i.ports:
+                opts += ['-L', f'{ip}:{port}:{ip}:{port}']
     command = ['ssh'] + opts + [jump_host]
     logger.info("\tCommand %s", command)
     subprocess.call(command)
